7344169f4a7ef6e3e35280d9b8c3dde0c7d95636
Saurabh Nema <sauravnemabhopal@gmail.com>
2017-08-11 13:53:21 +0530

    This is normal message that we get by a print method

---
+++ print hello.py
@@ -0,0 +1,13 @@
+class PrintHello():
+    """
+    This is a class which is used to print a hello on your console
+    """
+    def PrintSomething(self):
+        """
+        This is a print Function that we have define by using a def function
+        :return: nothing
+        """
+        print("This is it You have your first print statement with pyhton and few more to come so stay tuned") #This is a print function it's builtin function which is used to print as it is we declare in " " this.
+if __name__ == '__main__':
+    Print=PrintHello() # this is a class name and we are making a obj of it
+    Print.PrintSomething() # Then this object is calling a function that we have made in class
